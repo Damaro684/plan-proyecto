0c6f3c4255d300f0cf7305a002ff999dfb57cab6
Daniel <danielmateos684@gmail.com>
2025-01-31 09:47:19 +0100

    Primera parte clase 2

---
+++ Departamento.java
@@ -0,0 +1,24 @@
+public class Departamento
+{
+    private String nombreDpto;
+    private String descripcionDpto;
+    private Proyecto proyecto;
+
+    public Departamento(String nombre, String descripcion){
+        nombreDpto = nombre;
+        descripcionDpto = descripcion;
+    }
+    
+    public String getDepartamento(){
+        return nombreDpto;
+    }
+    
+    public String getDescripcion(){
+        return descripcionDpto;
+    }
+    
+    public void imprimirDetalles(){
+        System.out.println("Nombre departamento: " + nombreDpto + "\nDescripcion: " + descripcionDpto);
+    }
+}
+
