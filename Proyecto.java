e328ee3c755d35aa907444681aaee135e620afa9
hcc <hcc@gmail.com>
2025-01-31 09:35:31 +0100

    Añadimos los metodos y los detalles de clase proyecto

--- Proyecto.java
+++ Proyecto.java
@@ -1,4 +1,29 @@
-public class Proyecto
-{
+public class Proyecto{
     private String nombreProyecto;
+    private String descripcion;
+
+    public Proyecto(String proyecto, String descripcionProyecto){
+        nombreProyecto = proyecto;
+        descripcion = descripcionProyecto;
+    }
+    
+    public String getProyecto(){
+        return nombreProyecto;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public void setProyecto(String renombrado){
+        nombreProyecto = renombrado;
+    }
+    
+    public void setDescripcion(String actualizadoDescripcion){
+        descripcion = actualizadoDescripcion;
+    }
+    
+    public void imprimirDetalles(){
+        System.out.println("Nombre proyecto: " + nombreProyecto + "\nDescripcion: " + descripcion);
+    }
 }
